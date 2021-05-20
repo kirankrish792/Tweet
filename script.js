4dc342b5cb1de7f61e71f015f1891e2209c39fbb
Kiran KS <kirankrish792@gmail.com>
2021-05-21 00:16:49 +0530

    initial 8

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
 const form = document.querySelector('form');
+const name = document.querySelector('#name').value;
 form.addEventListener('submit', function (e) {
     e.preventDefault();
-    tweet();
+    const name = document.querySelector('#name').value;
+    const email = document.querySelector('#email').value;
+    const Tweet = document.querySelector('textarea').value;
+    if (name === '' || Tweet === '') {
+
+        if (document.querySelector('h5') === null) {
+            const h5 = document.createElement('h5');
+            h5.append("please enter a valid input")
+            form.prepend(h5);
+        }
+    }
+    else {
+        tweet();
+        if (document.querySelector('h5') !== null) {
+            document.querySelector('h5').remove();
+        }
+
+    }
+
     document.querySelector('#name').value = '';
     document.querySelector('#email').value = '';
     document.querySelector('textarea').value = '';
@@ -20,7 +39,7 @@ function tweet() {
     h2.append(name);
     h4.append(email);
     p.append(Tweet);
-    div.className = 'cont';
+    div.className = 'tweet';
     div2.className = "content";
     div2.append(h2, h4, p);
     div.append(div2);
